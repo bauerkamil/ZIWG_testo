e22646f716dbe0afc90623f2504dffae0e847bf1
kAleks12 <kacper4553@gmail.com>
2024-04-30 18:49:24 +0200

    Update config for deployment

--- Backend/src/dal/db.go
+++ Backend/src/dal/db.go
@@ -1,6 +1,7 @@
 package dal
 
 import (
+	"fmt"
 	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -8,22 +9,24 @@ import (
 	"os"
 )
 
-var loadError = godotenv.Load("../.env")
-var dsn = "host=" + os.Getenv("DB_HOST") +
-	" user=" + os.Getenv("DB_USER") +
-	" password=" + os.Getenv("DB_PASSWORD") +
-	" dbname=" + os.Getenv("DB_NAME") +
-	" port=" + os.Getenv("DB_PORT")
-var DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
-	NamingStrategy: schema.NamingStrategy{
-		TablePrefix:   "system.", // table name prefix, table for `User` would be `t_users`
-		SingularTable: true,      // use singular table name, table for `User` would be `user` with this option enabled
-	}})
+var DB *gorm.DB = nil
+var err error
 
 func init() {
+	var loadError = godotenv.Load("../.env")
 	if loadError != nil {
-		panic("Error loading .env file")
+		fmt.Println("Error loading .env file")
 	}
+	var dsn = "host=" + os.Getenv("DB_HOST") +
+		" user=" + os.Getenv("DB_USER") +
+		" password=" + os.Getenv("DB_PASSWORD") +
+		" dbname=" + os.Getenv("DB_NAME") +
+		" port=" + os.Getenv("DB_PORT")
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			TablePrefix:   "system.", // table name prefix, table for `User` would be `t_users`
+			SingularTable: true,      // use singular table name, table for `User` would be `user` with this option enabled
+		}})
 	if err != nil {
 		panic("failed to connect database")
 	}
