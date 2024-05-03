c153c506bdaa0213a27a30f348b82743c394975e
Damian0401 <szkola_abc@o2.pl>
2024-05-03 02:30:22 +0200

    Added endpoint for gettings user's courses to USOSConnector

---
+++ USOSConnector/USOSConnector.Functions/Triggers/CoursesTrigger.cs
@@ -0,0 +1,103 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+using USOSConnector.Functions.Attributes;
+using USOSConnector.Functions.Extensions;
+using USOSConnector.Functions.Options;
+using USOSConnector.Functions.Services.JwtService;
+using USOSConnector.Functions.Services.UsosService;
+using USOSConnector.Functions.Triggers.Dtos;
+
+namespace USOSConnector.Functions.Triggers;
+
+public class CoursesTrigger
+{
+    private readonly IMemoryCache _cache;
+    private readonly IUsosService _usosService;
+    private readonly IJwtService _jwtService;
+    private readonly TimeProvider _timeProvider;
+    private readonly USOSOptions _options;
+
+    public CoursesTrigger(
+        IMemoryCache cache,
+        IUsosService usosService,
+        IJwtService jwtService,
+        TimeProvider timeProvider,
+        IOptions<USOSOptions> options)
+    {
+        _cache = cache;
+        _usosService = usosService;
+        _jwtService = jwtService;
+        _timeProvider = timeProvider;
+        _options = options.Value;
+    }
+
+    [JwtAuthorize]
+    [Function(nameof(Courses))]
+    public async Task<HttpResponseData> Courses(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "courses")] 
+        HttpRequestData req,
+        CancellationToken cancellationToken)
+    {
+        var bearerToken = req.GetBearerToken();
+
+        var userClaims = _jwtService.GetUserClaims(bearerToken);
+
+        var tokenSecret = _cache.Get<string>(userClaims.UserId);
+
+        if (string.IsNullOrEmpty(tokenSecret))
+        {
+            // Token is cached as long as it is valid
+            return await req.CreateProblemResponseAsync(
+                HttpStatusCode.Unauthorized,
+                "Expired user token.");
+        }
+
+        var termsResult = await _usosService.GetCurrentUserTermsAsync(
+            userClaims.UsosToken,
+            tokenSecret,
+            cancellationToken);
+
+        var coursesResult = await _usosService.GetCurrentUserCoursesAsync(
+            userClaims.UsosToken,
+            tokenSecret,
+            cancellationToken);
+
+        var currentDate = _timeProvider.GetUtcNow();
+        var response = new CoursesResponseDto
+        {
+            Terms = termsResult.Terms
+                .Select(t => new CoursesResponseTerm
+                {
+                    Id = t.Id,
+                    Name = t.Name.Pl,
+                    StartDate = t.StartDate,
+                    EndDate = t.EndDate,
+                    IsCurrent = t.StartDate <= currentDate && t.EndDate >= currentDate,
+                    Courses = coursesResult.CourseEditions[t.Id]
+                        .Select(c => new CoursesResponseCourse
+                        {
+                            Id = c.CurseId,
+                            Name = c.CourseName.Pl,
+                            UsosUrl = c.UserGroups.First().CourseProfileUrl,
+                            Groups = c.UserGroups
+                                .Select(g => new CoursesResponseGroup
+                                {
+                                    Id = g.CourseUnitId,
+                                    TypeName = g.ClassType.Pl,
+                                    Lecturer = new CoursesResponseLecturer
+                                    {
+                                        Id = g.Lecturers.First().Id,
+                                        FirstName = g.Lecturers.First().FirstName,
+                                        LastName = g.Lecturers.First().LastName
+                                    }
+                        }).ToArray()
+                }).ToArray()
+            }).ToArray()
+        };
+
+        return await req.CreateOkResponseAsync(response);
+    }
+}
