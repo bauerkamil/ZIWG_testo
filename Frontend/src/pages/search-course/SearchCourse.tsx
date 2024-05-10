f99cafb382fb502bdd73b0351a9cf1f045d7e47f
Kamil Bauer <259102@student.pwr.edu.pl>
2024-05-10 12:12:51 +0200

    Added live filters for searching tests on frontend

--- Frontend/src/pages/search-course/SearchCourse.tsx
+++ Frontend/src/pages/search-course/SearchCourse.tsx
@@ -1,6 +1,7 @@
 import Client from "@/api/Client";
 import Navbar from "@/components/navbar/Navbar";
 import TestCard from "@/components/test-card/TestCard";
+import { Label } from "@/components/ui";
 import { Input } from "@/components/ui/input";
 import { NavbarPages } from "@/shared/enums/NavbarPages";
 import { ITest } from "@/shared/interfaces";
@@ -8,6 +9,10 @@ import { useState, useEffect } from "react";
 
 const SearchCourse: React.FC = () => {
   const [tests, setTests] = useState<ITest[]>([]);
+  const [filteredTests, setFilteredTests] = useState<ITest[]>([]);
+  const [nameFilter, setNameFilter] = useState<string>("");
+  const [teacherFilter, setTeacherFilter] = useState<string>("");
+  const [courseFilter, setCourseFilter] = useState<string>("");
 
   const handleDeleted = (id: string) => {
     setTests(tests.filter((test) => test.id !== id));
@@ -28,23 +33,88 @@ const SearchCourse: React.FC = () => {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    const splitTeacher = teacherFilter.split(" ");
+    let tests1 = tests;
+    if (splitTeacher.length > 0) {
+      tests1 = tests1.filter(
+        (t) =>
+          t.course?.teacher?.name
+            .toLowerCase()
+            .includes(splitTeacher[0].toLowerCase()) ||
+          t.course?.teacher?.surname
+            .toLowerCase()
+            .includes(splitTeacher[0].toLowerCase())
+      );
+    }
+    if (splitTeacher.length > 1) {
+      tests1 = tests1.filter(
+        (t) =>
+          t.course?.teacher?.name
+            .toLowerCase()
+            .includes(splitTeacher[1].toLowerCase()) ||
+          t.course?.teacher?.surname
+            .toLowerCase()
+            .includes(splitTeacher[1].toLowerCase())
+      );
+    }
+    if (nameFilter) {
+      tests1 = tests1.filter((t) =>
+        t.name?.toLowerCase().includes(nameFilter.toLowerCase())
+      );
+    }
+    if (courseFilter) {
+      tests1 = tests1.filter((t) =>
+        t.course?.name?.toLowerCase().includes(courseFilter.toLowerCase())
+      );
+    }
+    setFilteredTests(tests1);
+  }, [courseFilter, nameFilter, teacherFilter, tests]);
+
   return (
     <div className="flex min-h-screen w-full flex-col">
       <Navbar page={NavbarPages.SearchCourse} />
       <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
-        Wyszukaj swój boży kurs
-        <form>
-          <div>
+        <div className="grid gap-2 text-center">
+          <div className="text-4xl font-bold">
+            WYSZUKAJ SWÓJ ZBAWCZY TESTOWNIK
+          </div>
+          <p className="text-balance text-muted-foreground">
+            I skończ wreszcie udawać że się uczysz
+          </p>
+        </div>
+        <div className="flex flex-row gap-4">
+          <div className="grow">
+            <Label>Nazwa testo</Label>
             <Input
+              value={nameFilter}
+              onChange={(e) => setNameFilter(e.target.value)}
               type="search"
-              placeholder="Search ..."
-              className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
+              placeholder="Wyszukaj..."
             />
           </div>
-        </form>
+          <div className="grow">
+            <Label>Prowadzący</Label>
+            <Input
+              value={teacherFilter}
+              onChange={(e) => setTeacherFilter(e.target.value)}
+              type="search"
+              placeholder="Wyszukaj..."
+            />
+          </div>
+          <div className="grow">
+            <Label>Nazwa przedmiotu</Label>
+            <Input
+              value={courseFilter}
+              onChange={(e) => setCourseFilter(e.target.value)}
+              type="search"
+              placeholder="Wyszukaj..."
+            />
+          </div>
+        </div>
         <div className="text-2xl">Testowniki:</div>
         <div className="grid gap-4 md:grid-cols-2">
-          {tests.map((test) => (
+          {filteredTests.map((test) => (
             <TestCard key={test.id} test={test} onDeleted={handleDeleted} />
           ))}
         </div>
